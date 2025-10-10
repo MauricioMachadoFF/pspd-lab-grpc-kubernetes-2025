8d3239d05ed1b816cdbc1b7e2bfc4011be3e5102
Natan Tavares Santana <64803935+Neitan2001@users.noreply.github.com>
2025-10-10 12:18:23 -0300

    Correção do kubernetes (#5)
    * fix: correção do kubernetes
    
    * feat: criação do gateway
    
    * feat: add kubernetes manifests for api-gateway service
    
    * fix kubernets gateway
    
    * fix failing redirect grpc problem
    
    * fix: correção do kubernetes com o grpc
    
    ---------
    
    Co-authored-by: MauricioMachadoFF <mauriciofozmmff33@gmail.com>
    Co-authored-by: guilherme lima <guizinpeix7@gmail.com>

--- MicroserviceB_QRCode/MicroserviceB_QRCode/Services/QrcodeService.cs
+++ MicroserviceB_QRCode/MicroserviceB_QRCode/Services/QrcodeService.cs
@@ -1,55 +1,23 @@
 using Grpc.Core;
-using MicroserviceA_LinkShortener;
 using QRCoder;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.IO.Compression;
-using ZXing;
-using ZXing.Common;
-using ZXing.QrCode.Internal;
-using static QRCoder.PayloadGenerator;
 
 namespace MicroserviceB_QRCode.Services;
 
-public static class Repository
+public class QrcodeService : QrCodeGenerator.QrCodeGeneratorBase
 {
-    private static readonly Dictionary<string, string> _store = new();
-
-    public static void Add(string key, string value)
-    {
-        _store[key] = value;
-    }
-
-    public static string Get(string key)
-        => _store.TryGetValue(key, out var value) ? value : string.Empty;
-}
-
-public class QrcodeService : QrcodeGenerator.QrcodeGeneratorBase
-{
-    public override Task<CreateQRReply> CreateQR(CreateQRRequest request, ServerCallContext context)
+    public override Task<GenerateQrCodeReply> GenerateQrCode(GenerateQrCodeRequest request, ServerCallContext context)
     {
         using var qrGenerator = new QRCodeGenerator();
-        using var qrCodeData = qrGenerator.CreateQrCode(request.Url, QRCodeGenerator.ECCLevel.Q);
+        using var qrCodeData = qrGenerator.CreateQrCode(request.Text, QRCodeGenerator.ECCLevel.Q);
 
-        var qrCode = new BitmapByteQRCode(qrCodeData);
+        var qrCode = new PngByteQRCode(qrCodeData);
         byte[] qrCodeBytes = qrCode.GetGraphic(20); 
 
         string base64Qr = Convert.ToBase64String(qrCodeBytes);
 
-        Repository.Add(request.Url, base64Qr);
-
-        return Task.FromResult(new CreateQRReply
+        return Task.FromResult(new GenerateQrCodeReply
         {
             QrCodeBase64 = base64Qr
-        });
-    }
-
-    public override Task<DecodeQRReply> DecodeQR(DecodeQRRequest request, ServerCallContext context)
-    {
-       return Task.FromResult(new DecodeQRReply
-        {
-            Url = Repository.Get(request.QrCodeBase64)
         });
     }
 }
